a790f65ebdde98cf34d07dda031137d2a48f5ce2
Alex Garella <agarella@gmail.com>
2015-02-16 21:57:15 +0100

    Rename Observer to Observable

--- src/main/scala/com/alexgarella/RxKinesis/RxKinesisObservable.scala
+++ src/main/scala/com/alexgarella/RxKinesis/RxKinesisObservable.scala
@@ -2,7 +2,7 @@ package com.alexgarella.RxKinesis
 
 import rx.lang.scala.{Subscriber, Subscription, Observable}
 
-trait RxKinesisObserver[T] extends Observable[T] {
+trait RxKinesisObservable[T] extends Observable[T] {
 
   override def subscribe(subscriber: Subscriber[T]): Subscription
 
--- src/main/scala/com/alexgarella/RxKinesis/internal/RxKinesis.scala
+++ src/main/scala/com/alexgarella/RxKinesis/internal/RxKinesis.scala
@@ -1,10 +1,10 @@
 package com.alexgarella.RxKinesis.internal
 
-import com.alexgarella.RxKinesis.RxKinesisObserver
+import com.alexgarella.RxKinesis.RxKinesisObservable
 import com.amazonaws.services.kinesis.AmazonKinesisAsyncClient
 import rx.lang.scala.{Observer, Subscription}
 
-class RxKinesis[T] extends RxKinesisObserver[T]{
+class RxKinesis[T] extends RxKinesisObservable[T]{
 
   lazy val kinesis = initialize
 
