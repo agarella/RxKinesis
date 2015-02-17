79df27e666f581fe1eb82cae27d85196d698c154
Alex Garella <agarella@gmail.com>
2015-02-17 22:31:50 +0100

    [WIP] Write data to stream.

--- src/main/scala/com/alexgarella/RxKinesis/internal/RxKinesis.scala
+++ src/main/scala/com/alexgarella/RxKinesis/internal/RxKinesis.scala
@@ -1,15 +1,43 @@
 package com.alexgarella.RxKinesis.internal
 
+import java.nio.ByteBuffer
+
 import com.amazonaws.services.kinesis.AmazonKinesisAsyncClient
+import com.amazonaws.services.kinesis.model.{GetRecordsRequest, PutRecordRequest}
 import rx.lang.scala.Observable
 
-object RxKinesis{
+import scala.collection.JavaConversions._
+import scala.util.{Failure, Success, Try}
 
-  lazy val kinesis = new AmazonKinesisAsyncClient()
+object RxKinesis {
 
-  def kinesisObservable: Observable[String] = {
+  private val kinesis = new AmazonKinesisAsyncClient()
+
+  def writeData(data: ByteBuffer): Unit = {
+    Try {
+      val putRecord = new PutRecordRequest
+      putRecord.setStreamName("test")
+      putRecord.setPartitionKey("0")
+      putRecord.setData(data)
+      kinesis.putRecord(putRecord)
+    } match {
+      case Success(_)            => ()
+      case Failure(e: Throwable) => throw e
+    }
+  }
+
+  def kinesisObservable(): Observable[String] = {
     Observable[String](
-      subscriber => (1 to 10).map(_.toString).toList.foreach(subscriber.onNext)
+      subscriber => {
+        val shardId = kinesis.describeStream("test").getStreamDescription.getShards.toList.head.getShardId
+        val shardIterator = kinesis.getShardIterator("test", shardId, "LATEST").getShardIterator
+        val getRecordsRequest = new GetRecordsRequest()
+        getRecordsRequest.setShardIterator(shardIterator)
+        val records = kinesis.getRecords(getRecordsRequest)
+        records.getRecords.toList.foreach { r =>
+            subscriber.onNext(r.getData.asIntBuffer().get().toString)
+        }
+      }
     )
   }
 
--- src/test/scala/com/alexgarella/RxKinesis/internal/RxKinesisTest.scala
+++ src/test/scala/com/alexgarella/RxKinesis/internal/RxKinesisTest.scala
@@ -1,9 +1,12 @@
 package com.alexgarella.RxKinesis.internal
 
+import java.nio.ByteBuffer
+
 import org.scalatest.mock.MockitoSugar
 import org.scalatest.{FeatureSpec, GivenWhenThen}
-
 import rx.lang.scala.Observer
+
+import scala.util.Random
 
 class RxKinesisTest extends FeatureSpec with GivenWhenThen with MockitoSugar {
 
@@ -15,11 +18,19 @@ class RxKinesisTest extends FeatureSpec with GivenWhenThen with MockitoSugar {
     And("an observer")
     val observer = new Observer[String]{
       override def onNext(value: String): Unit = println(s"onNext: $value")
+      override def onError(error: Throwable): Unit = println(error.getMessage)
     }
 
     When("subscribing")
 
     Then("the code of the observer will be executed async")
+
+    1 to 10 foreach { _ =>
+      val b = ByteBuffer.allocate(10)
+      b.putInt((new Random).nextInt())
+      RxKinesis.writeData(b)
+    }
+
     rxKinesis.subscribe(observer)
   }
 }
