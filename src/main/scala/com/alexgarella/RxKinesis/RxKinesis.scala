86b285e5671070b077f30b50d63bf3fa3bd76c62
Alex Garella <agarella@gmail.com>
2015-03-26 21:40:09 +0100

    Add Licenses to all files. Implement toString method in RxKinesis.

--- src/main/scala/com/alexgarella/RxKinesis/RxKinesis.scala
+++ src/main/scala/com/alexgarella/RxKinesis/RxKinesis.scala
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2014 Alex Garella
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
 package com.alexgarella.RxKinesis
 
 import com.alexgarella.RxKinesis.RecordProcessor.{KinesisRecordProcessor, RecordProcessorFactory}
@@ -16,13 +31,15 @@ class RxKinesis(kclConfig: KinesisClientLibConfiguration) extends Logging {
       recordProcessor.subscribe(subscriber)
     }
   } doOnUnsubscribe  {
-    Log.info(s"Stopping stream: ${kclConfig.getStreamName}")
+    Log.info(s"Stopping: $this")
     stop()
   }
 
   def stream(): Unit = worker.run()
 
   def stop(): Unit = worker.shutdown()
+
+  override def toString = s"RxKinesis, stream name: ${kclConfig.getStreamName}, application name: ${kclConfig.getApplicationName}"
 }
 
 object RxKinesis {
