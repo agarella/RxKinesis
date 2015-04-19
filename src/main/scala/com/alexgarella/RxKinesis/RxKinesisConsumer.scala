41cb9999c2f32c53ef9dca85fe66d39ce13de8a1
Alex Garella <agarella@gmail.com>
2015-04-19 15:34:10 +0200

    Update License

--- src/main/scala/com/alexgarella/RxKinesis/RxKinesisConsumer.scala
+++ src/main/scala/com/alexgarella/RxKinesis/RxKinesisConsumer.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014 Alex Garella
+ * Copyright 2015 Alex Garella
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
